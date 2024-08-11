759b52f5dcf20a9d36f370067a371e14d47abdb0
Công Hoàng <23664101.hoang@student.iuh.edu.vn>
2024-08-11 20:02:38 +0700

    first commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Ho Cong Hoang!");
+        System.out.println("Welcome to KTPM19A!");
+    }
+}
